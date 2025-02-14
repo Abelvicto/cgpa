c49e37a2a6e439c9f4f522e5e224f37e1bce1a02
Abelvicto <104443702+Abelvicto@users.noreply.github.com>
2025-02-14 12:07:25 +0100

    Update getBooks.js

--- netlify/functions/getBooks.js
+++ netlify/functions/getBooks.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 exports.handler = async function () {
     // Load Cloudinary credentials from environment variables
